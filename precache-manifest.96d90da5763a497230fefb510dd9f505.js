812e88de8e452b5e9406dc0343ef1f69f8642ad1
Dias Abishev <abishev295@gmail.com>
2020-09-21 11:50:19 +0600

    Updates

---
+++ precache-manifest.96d90da5763a497230fefb510dd9f505.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f146941a41461671cfe8a84b73d70988",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d85f17ef2a5b83dfcde",
+    "url": "/static/css/main.73f3c5b6.chunk.css"
+  },
+  {
+    "revision": "6171c139058b5d2d8b29",
+    "url": "/static/js/2.baf51eaa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.baf51eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d85f17ef2a5b83dfcde",
+    "url": "/static/js/main.cfd67fb5.chunk.js"
+  },
+  {
+    "revision": "f881c1458a1090b43a7b",
+    "url": "/static/js/runtime-main.97773ce4.js"
+  },
+  {
+    "revision": "97c6953233b192dccad772843695a5d5",
+    "url": "/static/media/cam_dark.97c69532.svg"
+  },
+  {
+    "revision": "1182b96fea77cb78388f0c86ab86c7d1",
+    "url": "/static/media/cam_white.1182b96f.svg"
+  }
+]);
